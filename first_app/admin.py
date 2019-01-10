bdcb70df35d1e77e2dd0b6fce81bb22585eb3f19
rini pandey <rinipandey1910@gmail.com>
2019-01-10 20:56:25 +0530

    databse modified, style added

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Register
+from .models import Register, Train, Ticket, Station, Passenger
 # Register your models here.
 
 admin.site.register(Register)
+admin.site.register(Train)
+admin.site.register(Ticket)
+admin.site.register(Passenger)
+admin.site.register(Station)
--- first_app/models.py
+++ first_app/models.py
@@ -16,4 +16,48 @@ class Register(models.Model):
     password = models.CharField(max_length=15)
     varify_password = models.CharField(max_length=15)
 
- 
+
+class Train(models.Model):
+    train_no = models.IntegerField(primary_key=True)
+    train_name = models.CharField(max_length=30)
+    destination = models.CharField(max_length=30)
+    source = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.train_name
+
+    def __str__(self):
+        return self.train_name
+
+
+class Station(models.Model):
+    station_name = models.CharField(max_length=30)
+    train_no = models.ForeignKey( Train, on_delete=models.CASCADE)
+    arival_time = models.CharField(max_length=30)
+    depetrure_time = models.CharField(max_length=30)
+    platform = models.IntegerField()
+
+    def __unicode__(self):
+        return self.station_name
+
+    def __str__(self):
+        return self.station_name
+
+
+
+class Ticket(models.Model):
+    pnr = models.IntegerField(primary_key=True)
+    train_no = models.ForeignKey(Train, on_delete=models.CASCADE)
+    train_name = models.CharField(max_length=30)
+    coach_no = models.CharField(max_length=10)
+    seat_no = models.IntegerField()
+    from_station = models.CharField(max_length=30)
+    to_station = models.CharField(max_length=30)
+    fair = models.IntegerField()
+    d_o_j = models.DateField()
+
+class Passenger(models.Model):
+    name = models.CharField(max_length=30)
+    pnr =  models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField()
+    gender = models.ForeignKey(Register, on_delete=models.CASCADE)
